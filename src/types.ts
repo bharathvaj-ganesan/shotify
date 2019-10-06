db6b2d545674f0c4f9f0ce0751f222df5d8cf029
Bharathvaj Ganesan <bharathvaj1995@gmail.com>
2019-10-06 22:02:19 +0530

    wip: add setup methods

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface HTML2CanvasOptions {
+    async?: boolean;
+    allowTaint?: boolean;
+    backgroundColor?: string;
+    canvas?: HTMLCanvasElement;
+    foreignObjectRendering?: boolean;
+    imageTimeout?: number;
+    logging?: boolean;
+    proxy?: string;
+    removeContainer?: boolean;
+    scale?: number;
+    useCORS?: boolean;
+    width?: number;
+    height?: number;
+    x?: number;
+    y?: number;
+    scrollX?: number;
+    scrollY?: number;
+    windowWidth?: number;
+}
+
+export interface ShotOptions {
+    html2canvasOptions?: HTML2CanvasOptions;
+    previewContainer: HTMLElement;
+    ignoreElements?: HTMLElement[];
+}
+
+export interface ShotArea {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
